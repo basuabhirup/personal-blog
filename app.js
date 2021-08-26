18a6c29b31a9c5dae9168a014b93b5f5518edbd4
basuabhirup <basu.abhirup@yahoo.com>
2021-08-26 17:45:13 +0530

    Add skeletal structure of the Project.

--- app.js
+++ app.js
@@ -13,16 +13,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render('home', {homeStarting: homeStartingContent});
+})
 
+app.get("/about", (req, res) => {
+  res.render('about', {aboutStarting: aboutContent});
+})
 
-
-
-
-
-
-
-
-
+app.get("/contact", (req, res) => {
+  res.render('contact', {contactStarting: contactContent});
+})
 
 
 
